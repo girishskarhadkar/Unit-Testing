9237d86d7b39dff324db771f7bddf4982cdae69a
girishskarhadkar <girishskarhadkar.gmail.com>
2024-09-28 09:49:43 +0530

    added a.txt

--- Part 5 - Unit Testing/Unit_Test_1/1_Unit_Test.py
+++ Part 5 - Unit Testing/Unit_Test_1/1_Unit_Test.py
@@ -3,10 +3,22 @@
 #- Function under development
 #- Function to test
 #################################################
+products = [
+    {"id": 1, "name": "Product 1", "price": 10, "description": "Description 1"},
+    {"id": 2, "name": "Product 2", "price": 20, "description": "Description 2"},
+    {"id": 3, "name": "Product 3", "price": 30, "description": "Description 3"},
+]
+
+def test_get_products():
+    product = next((p for p in products if p['id'] == 4), None)
+    if product:
+        assert True
+    else:
+        assert False
 
 
 def func(x):
     return x + 1
 	
 def test_answer():
-    assert func(3) == 4
+
